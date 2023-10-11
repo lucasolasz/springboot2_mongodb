74080a301a4ffdd62e9e00be0f5a4e11706fd221
conuaz <lucas.olasz@tty2000.com.br>
2023-10-11 18:30:23 -0300

    primeiro commit

---
+++ springboot2_mongodb/src/test/java/com/educandoweb/springboot2_mongodb/Springboot2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springboot2_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
