318888b35e017b8c917db155d705ffbfcd3934c2
conuaz <lucas.olasz@tty2000.com.br>
2023-10-23 16:03:11 -0300

    Criando entity Post com User aninhado

--- springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/config/Instantiation.java
+++ springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/config/Instantiation.java
@@ -1,30 +1,46 @@
 package com.educandoweb.springboot2_mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.springboot2_mongodb.domain.Post;
 import com.educandoweb.springboot2_mongodb.domain.User;
+import com.educandoweb.springboot2_mongodb.repository.PostRepository;
 import com.educandoweb.springboot2_mongodb.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		repo.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		repo.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+	
 
 	}
 }
