65ad37f109b49a107da7fedde66c31cf97f1bcfd
conuaz <lucas.olasz@tty2000.com.br>
2023-10-23 14:56:54 -0300

    Inserção de usuário com POST

--- springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/services/UserService.java
+++ springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/services/UserService.java
@@ -7,23 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.springboot2_mongodb.domain.User;
+import com.educandoweb.springboot2_mongodb.dto.UserDTO;
 import com.educandoweb.springboot2_mongodb.repository.UserRepository;
 import com.educandoweb.springboot2_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
