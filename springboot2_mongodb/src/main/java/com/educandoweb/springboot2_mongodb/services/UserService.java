8da4c04fb56cb7250cf9976dffb1ccb985877aa0
conuaz <lucas.olasz@tty2000.com.br>
2023-10-23 15:01:54 -0300

    Deleção de usuário com DELETE

--- springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/services/UserService.java
+++ springboot2_mongodb/src/main/java/com/educandoweb/springboot2_mongodb/services/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 
+	public void delete(String id) {
+		//Já existe tratamento de excessão para esse ID
+		this.findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
